4cfe5190930c7daf966e431477cdcb8028d38f94
Eklak Dangaura <azens1995@gmail.com>
2022-08-30 16:17:49 +0545

    Initialize todo project

---
+++ src/main/java/dev/eklak/todospring/TodoSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.eklak.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/eklak/todospring/TodoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eklak.todospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
